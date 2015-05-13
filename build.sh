cb6ff75a5c5bca94261d16569020d91dd6771453
James Logan <jamesblogan20@gmail.com>
2015-05-13 13:38:15 -0500

    babel compiling

--- build.sh
+++ build.sh
@@ -12,3 +12,12 @@ cd ..
 rm -rf src/**/*.html
 rm -rf public/**/_*.html
 rm -rf public/_partials
+
+#complie scss to css
+./node_modules/.bin/node-sass \
+ --output-style compressed \
+ --source-map-embed \
+ src/_styles/main.scss public/css/main.css
+
+#conver ES6 JS TO ES5
+./node_modules/.bin/babel src --out-dir public -s inline
